dcaa33dedc4e3b0a064c5c38bce5cf2734b65f98
Patrik Potocki <rambo3swe@gmail.com>
2009-10-03 15:56:53 +0200

    BaseController.cs
    Signed-off-by: Patrik Potocki <rambo3swe@gmail.com>

--- src/vbgCMS.UI.Web/Code/Mvc/Controllers/BaseController.cs
+++ src/vbgCMS.UI.Web/Code/Mvc/Controllers/BaseController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace vbgCMS.UI.Web.Code.Mvc.Controllers
 {
+    [HandleError]
     public class BaseController : NHibernateController
     {
         public  BaseController()
