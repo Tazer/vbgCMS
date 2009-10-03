592584fe80555baf06a76f32e928d217f2408423
Patrik Potocki <rambo3swe@gmail.com>
2009-10-03 14:59:59 +0200

    Added NHibernateControllerBase.cs for transaction handling and session
    Signed-off-by: Patrik Potocki <rambo3swe@gmail.com>

---
+++ src/vbgCMS.UI.Web/Code/NHibernate/NHibernateControllerBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Practices.ServiceLocation;
+using NHibernate;
+
+namespace vbgCMS.UI.Web.Code.NHibernate
+{
+    public abstract class NHibernateControllerBase : Controller
+    {
+        private ITransaction _transaction;
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            _transaction = ServiceLocator.Current.GetInstance<ISession>().BeginTransaction();
+            base.OnActionExecuting(filterContext);
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //_transaction = ServiceLocator.Current.GetInstance<ISession>().Transaction;
+
+            try
+            {
+                _transaction.Commit();
+            }
+            catch (HibernateException)
+            {
+                _transaction.Rollback();
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
