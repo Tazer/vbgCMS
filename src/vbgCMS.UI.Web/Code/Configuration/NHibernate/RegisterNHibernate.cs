28f752b777b0482463137e8adcf35948978e537d
mabe <magnus.bertilsson@gmail.com>
2009-10-05 19:16:01 +0200

    Added cache provier dll and added to configuration.
    Signed-off-by: mabe <magnus.bertilsson@gmail.com>

--- src/vbgCMS.UI.Web/Code/Configuration/NHibernate/RegisterNHibernate.cs
+++ src/vbgCMS.UI.Web/Code/Configuration/NHibernate/RegisterNHibernate.cs
@@ -10,6 +10,7 @@ using Cfg = NHibernate.Cfg;
 using Microsoft.Practices.ServiceLocation;
 using NHibernate;
 using NHibernate.Context;
+using Caches = NHibernate.Caches;
 
 namespace vbgCMS.UI.Web.Code.Configuration.NHibernate
 {
@@ -75,28 +76,31 @@ namespace vbgCMS.UI.Web.Code.Configuration.NHibernate
 
     public class CustomDatabaseConfiguration
     {
+
+
         public static IPersistenceConfigurer Standard()
         {
             var connection = ConfigurationManager.ConnectionStrings["vbg_cms"];
 
+            //Findout how to set it by string, get it from appsettings
             switch (connection.ProviderName)
             {
                 case "MSSQL2008":
                     return
                         MsSqlConfiguration.MsSql2008.ConnectionString(
-                            x => x.Is(connection.ConnectionString));
+                            x => x.Is(connection.ConnectionString)).Cache(x => x.ProviderClass<Caches.SysCache.SysCacheProvider>());
                 case "MSSQL2005":
                     return MsSqlConfiguration.MsSql2005.ConnectionString(
-    x => x.Is(connection.ConnectionString));
+                            x => x.Is(connection.ConnectionString)).Cache(x => x.ProviderClass<Caches.SysCache.SysCacheProvider>());
                 case "MYSQL":
                     return MySQLConfiguration.Standard.ConnectionString(
-                            x => x.Is(connection.ConnectionString));
+                            x => x.Is(connection.ConnectionString)).Cache(x => x.ProviderClass<Caches.SysCache.SysCacheProvider>());
                 case "ORACLE10":
                     return OracleDataClientConfiguration.Oracle10.ConnectionString(
-                            x => x.Is(connection.ConnectionString));
+                            x => x.Is(connection.ConnectionString)).Cache(x => x.ProviderClass<Caches.SysCache.SysCacheProvider>());
                 case "SQLITE":
                     return SQLiteConfiguration.Standard.ConnectionString(
-    x => x.Is(connection.ConnectionString));
+                            x => x.Is(connection.ConnectionString)).Cache(x => x.ProviderClass<Caches.SysCache.SysCacheProvider>());
 
             }
             throw new ArgumentException("Provider not supported , please check providerName");
