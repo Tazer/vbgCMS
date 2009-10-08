1ace67d2e468263be40355221d35001b1a8bcc75
mabe <magnus.bertilsson@gmail.com>
2009-10-08 21:10:56 +0200

    ---
    Signed-off-by: mabe <magnus.bertilsson@gmail.com>

---
+++ src/vbgCMS/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace vbgCMS
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable)
+            {
+                action.Invoke(item);
+            }
+        }
+
+        public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.Count() == 0;
+        }
+    }
+}
---
+++ src/vbgCMS/Infrastructure/CMS/Interfaces/IPageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using vbgCMS.Infrastructure.NHibernate.Interfaces;
+
+namespace vbgCMS.Infrastructure.CMS.Interfaces
+{
+    public interface IPageRepository : IGet<Page>, ILoad<Page, long>, IGetById<Page, long>
+    {
+
+    }
+}
