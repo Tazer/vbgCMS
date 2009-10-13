c26a823255755f6bf6c345025df5c05fe4c23183
mabe <magnus.bertilsson@gmail.com>
2009-10-13 10:53:14 +0200

    Refactored validationfactory.
    Signed-off-by: mabe <magnus.bertilsson@gmail.com>

--- src/vbgCMS.UI.Web/Code/Configuration/Validation/StructureMapValidationFactory.cs
+++ src/vbgCMS.UI.Web/Code/Configuration/Validation/StructureMapValidationFactory.cs
@@ -14,24 +14,14 @@ namespace vbgCMS.UI.Web.Code.Configuration.Validation
 
 		public IValidator GetValidator(Type type)
 		{
-			//Do this becuse it tries to find validators for types like string. Needs a better solution.
-			if (!type.IsSubclassOf(typeof(BaseEntity)))
-				return null;
-
 			var serviceType = typeof(IValidator<>).MakeGenericType(new Type[] { type });
 
-			return (IValidator)ObjectFactory.GetInstance(serviceType);
+			return (IValidator)ObjectFactory.TryGetInstance(serviceType);
 		}
 
 		public IValidator<T> GetValidator<T>()
 		{
-			//return (IValidator<T>)GetValidator(typeof(T));
-
-			//Do this becuse it tries to find validators for types like string. Needs a better solution.
-			if (!typeof(T).IsSubclassOf(typeof(BaseEntity)))
-				return null;
-
-			return ObjectFactory.GetInstance<IValidator<T>>();
+			return ObjectFactory.TryGetInstance<IValidator<T>>();
 		}
 
 		#endregion
